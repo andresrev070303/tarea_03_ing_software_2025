fd6fc5c47157bcb3dc936be0144af1199629aa88
Andres Revollo <andres.revollo@ucb.edu.bo>
2025-02-17 15:38:24 -0400

    feat: saludo por genero

--- src/presenter.js
+++ src/presenter.js
@@ -2,6 +2,7 @@ import saludar from "./sumador";
 import saludarHora from "./saludarHora";
 
 const nombre_input = document.querySelector("#nombre");
+const genero_select = document.querySelector("#genero");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -9,6 +10,7 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const nombre = nombre_input.value;
+  const genero = genero_select.value;
 
-  div.innerHTML = "<p>" + saludarHora() + saludar(nombre) + "</p>";
+  div.innerHTML = "<p>" + saludarHora() + saludar(nombre, genero) + "</p>";
 });
--- src/sumador.js
+++ src/sumador.js
@@ -1,5 +1,11 @@
-function saludar(nombre) {
-  return nombre;
+function saludar(nombre, genero) {
+  if (genero === "M") {
+    return "Sr. " + nombre;
+  } else if (genero === "F") {
+    return "Sra. " + nombre;
+  } else {
+    return nombre;
+  }
 }
 
 export default saludar;
