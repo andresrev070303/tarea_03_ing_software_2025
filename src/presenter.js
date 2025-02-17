8134b60ee9f40bbecb9c31a8a4a1c8ba862aa5cd
Andres Revollo <andres.revollo@ucb.edu.bo>
2025-02-17 15:42:17 -0400

    feat: saludo por edades

--- src/presenter.js
+++ src/presenter.js
@@ -3,6 +3,7 @@ import saludarHora from "./saludarHora";
 
 const nombre_input = document.querySelector("#nombre");
 const genero_select = document.querySelector("#genero");
+const edad_input = document.querySelector("#edad");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -11,6 +12,7 @@ form.addEventListener("submit", (event) => {
 
   const nombre = nombre_input.value;
   const genero = genero_select.value;
+  const edad = edad_input.value;
 
-  div.innerHTML = "<p>" + saludarHora() + saludar(nombre, genero) + "</p>";
+  div.innerHTML = "<p>" + saludarHora() + saludar(nombre, genero, edad) + "</p>";
 });
